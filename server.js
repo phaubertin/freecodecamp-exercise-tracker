a55d07cb43c849c6fe0d3bf298767407462f81d2
Philippe Aubertin <philippe.aubertin@gmail.com>
2020-02-17 18:01:44 -0500

    Add tracker.js. Rework error handling.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const express = require('express');
 const mongoose = require('mongoose');
+const tracker = require('./tracker');
 
 /* Load environment variable from .env file. */
 dotenv.config();
@@ -27,29 +28,8 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-// Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
-
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage
-
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
+app.use('/api', tracker.apiErrorHandler);
+app.use(tracker.errorHandler);
 
 const listener = app.listen(process.env.PORT || defaults.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port)
---
+++ tracker.js
@@ -0,0 +1,81 @@
+/* Extract error HTTP status and message from error object. */
+function getErrorStatusAndMessage(err) {
+    let errorStatusAndMessage = function(status, message) {
+        return {
+            status : status,
+            message : message
+        }
+    }
+    
+    if(err.errors) {
+        /* This is a mongoose validation error.
+         * 
+         * Report the first validation error. */
+        const keys = Object.keys(err.errors)
+        return errorStatusAndMessage(400, err.errors[keys[0]].message);
+    }
+    else {
+        let status, message;
+        
+        if(err.status) {
+            status = err.status;
+        }
+        else {
+            status = 500;
+        }
+        
+        if(err.message) {
+            return errorStatusAndMessage(status, err.message);
+        }
+        else {
+            switch(status) {
+            case 400:
+                return errorStatusAndMessage(status, 'Bad request');
+            case 404:
+                return errorStatusAndMessage(status, 'Not found');
+            default:
+                return errorStatusAndMessage(status, 'Server error');
+            }
+        }
+    }
+}
+
+function catchNotFound(req, res, next) {
+    next({status : 404});
+}
+
+module.exports.catchNotFound = catchNotFound;
+
+/* This is a stack of two middleware functions. */
+module.exports.apiErrorHandler = [
+    /* If a response has not been sent yet, set not found error. */
+    catchNotFound,
+    /* Error handling function: send a JSON (API) response with an error member
+     * that contains the error message. */
+    function(err, req, res, next) {
+        let {status, message} = getErrorStatusAndMessage(err);
+        res.status(status).json({
+            error : message
+        });
+    }];
+
+/* Stack of two middleware functions as apiErrorHandler above, but this one
+ * sends an HTML error document instead of an API response. */
+module.exports.errorHandler = [
+    catchNotFound,
+    function(err, req, res, next) {
+        let {status, message} = getErrorStatusAndMessage(err);
+        
+        /* Try to send an HTML that has the HTTP status code in the name (e.g. 404.html). */
+        res.status(status).sendFile(__dirname + '/views/' + status + '.html', function(err) {
+            /* If that fails, send the HTML document for 500 Internal Server Error instead. */
+            if(err) {
+                res.sendFile(__dirname + '/views/500.html', function(err) {
+                    /* If all else fails, send a plain text error message. */
+                    if(err) {
+                        res.type('text/plain').send('Error ('+ status + '): ' + message);
+                    }
+                });
+            }
+        });
+    }];
